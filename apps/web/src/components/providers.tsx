435dd2ae997f0c5e35368c2eab1c004c0b38b740
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2026-06-20 16:10:17 +0200

    Updated providers and deledted unecessary file

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -7,7 +7,17 @@ import { ThemeProvider } from "./theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      // next-themes injects a <script> for pre-hydration theme detection;
+      // React 19 logs a false-positive "script tag" warning for it unless
+      // it's typed as non-executable. Known issue:
+      // https://github.com/pacocoursey/next-themes/issues/387
+      scriptProps={{ type: "application/json" }}
+    >
       <MotionConfig reducedMotion="user">
         {children}
         <Toaster richColors />
